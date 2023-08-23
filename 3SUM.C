d0a850f0f0c0a25fa3e5a00d9e001c1e232c8671
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-08-23 12:03:26 +0530

    3 Sum of three numbers
    This is C programming code for sum of three numbers

---
+++ 3SUM.C
@@ -0,0 +1,17 @@
+//Addition of three numbers
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	  int a,b,c,sum;
+	  clrscr();
+	  printf("enter the value of a=");
+	  scanf("%d",&a);
+	  printf("enter the value of b=");
+	  scanf("%d",&b);
+	  printf("enter the value of c=");
+	  scanf("%d",&c);
+	  sum=a+b+c;
+	  printf("\n sum of all numbers=%d",sum);
+	  getch();
+}
