69fe7c13eefbf6f22450796a2c375eb98b362293
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-08-24 11:31:01 +0530

    4 Printing a number
    This is a C Program for printing a number

---
+++ 4NUM.C
@@ -0,0 +1,12 @@
+//New Program
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	    int a;
+	    clrscr();
+	    printf("please enter any number=");
+	    scanf("%d",&a);
+	    printf("enter value of a=%d",a);
+	    getch();
+}
