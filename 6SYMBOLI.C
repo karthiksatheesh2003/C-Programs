9cb18496f59a28381a48519e5423a83b52e58922
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-08-24 11:34:02 +0530

    6 Symbolic Constant
    A C Program which expresses usage of symbolic constant and initialization of a variable

---
+++ 6SYMBOLI.C
@@ -0,0 +1,12 @@
+//Practice Work 1
+#include<stdio.h>
+#include<conio.h>
+#define A 10    // Symbolic Constant
+void main()
+{
+	int b=20;  //initialisation of variable
+	int c=A+b;
+	clrscr();
+	printf("sum=%d",c);
+	getch();
+}
