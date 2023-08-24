9328a8214054e9e5aaa69908faaec9296fdb42e2
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-08-24 11:32:21 +0530

    5 Variable Declaration
    A C Program for expressing variable declaration

---
+++ 5VARIABL.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+       int num; /* Variable declaration */
+       clrscr();
+       printf("Enter a number: \n");
+       scanf("%d",&num);
+       printf("You have entered %d",num);
+}
