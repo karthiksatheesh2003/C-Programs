e7378679848597a91fd2a7acf944404ecf123311
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-09-01 08:51:45 +0530

    9 Division
    This is a simple C Program to find the division of two numbers

---
+++ 9DIVISIO.C
@@ -0,0 +1,15 @@
+//Division of two numbers
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+      int a,b,division;
+      clrscr();
+      printf("enter the value of a=");
+      scanf("%d",&a);
+      printf("enter the value of b=");
+      scanf("%d",&b);
+      division=a/b;
+      printf("\n division of two numbers=%d",division);
+      getch();
+}
