0bf0ea762f4f1cae0e03d2240ace9a8068d9ed8d
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2023-09-01 08:52:54 +0530

    10 Size of Variables
    This is a simple C Program used to print the size of different data variables

---
+++ 10SIZE.C
@@ -0,0 +1,16 @@
+//Size of all data variables
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	    char a;
+	    int b;
+	    float c;
+	    double d;
+	    clrscr();
+	    printf("\n size of a=%d",sizeof(a));
+	    printf("\n size of b=%d",sizeof(b));
+	    printf("\n size of c=%d",sizeof(c));
+	    printf("\n size of d=%d",sizeof(d));
+	    getch();
+}
