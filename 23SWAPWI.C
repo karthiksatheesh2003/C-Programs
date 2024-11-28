48da89748e63ca9ab7157f0e46b4ba6b77b0119e
karthiksatheesh2003 <140892327+karthiksatheesh2003@users.noreply.github.com>
2024-11-28 18:25:44 +0000

    23 Swap two numbers using third variable
    This is a C Program to Swap two numbers using third variable

---
+++ 23SWAPWI.C
@@ -0,0 +1,14 @@
+//Swap two numbers using third variable
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10,b=20,c;
+	clrscr();
+	c=a;
+	a=b;
+	b=c;
+	printf("\n a is: %d",a);
+	printf("\n b is: %d",b);
+	getch();
+}
